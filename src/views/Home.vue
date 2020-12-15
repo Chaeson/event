d51251e85c5b11e231c5541f2ddebd056fd8c9c7
Chae JongWook <only9984@gmail.com>
2020-12-15 23:40:01 +0900

    - 원인:   Vue 2.0 에서 Vue 3.0 으로 변경되면서 style적용하는 부분이 변경됨.   해당 변경사항 확인 필요.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img class="background"
-          :id="background"
+          id="background"
            src="https://images.unsplash.com/photo-1509599589979-3b5a15d2816e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"/>
     <img class="foreground"
-          :id="foreground"
+          id="foreground"
            src="http://3.bp.blogspot.com/-gTf6EpGgHbs/TiswK4aeDlI/AAAAAAAAAKs/c6lOK9Ipigk/s400/creative+page+borders-10.png"/>
-    <div class="section section-1"  :id="first">
+    <div class="section section-1"  id="first" v-bind:style="{opacity: 100, offsetHeight: 100}">
       <div>
         <h1>Test</h1>
       </div>
     </div>
-    <div class="section section-2" :id="second">
+    <div class="section section-2" id="second" >
     <div>
       <h2> Here's more info </h2>
       <p> Test 2 </p>
@@ -22,21 +22,24 @@
 
 <script>
 // @ is an alias to /src
-import ref from 'vue';
 
 export default {
   beforeCreate () {
   },
   name: "Home",
   props:{
-
+    foreground:HTMLDocument,
+    background:HTMLDocument,
+    first:{
+      value:{
+        opacity:100,
+        offsetHeight:100
+      }
+    },
+    second:HTMLDocument
   },
   data: () => ({
     //
-    foreground:ref(null),
-    background:ref(null),
-    first:ref(null),
-    second:ref(null)
   }),
   mounted() {
     document.addEventListener('scroll', this.handleScroll)
@@ -47,10 +50,11 @@ export default {
   methods:{
     handleScroll(){
       const scrollY = window.scrollY
-      console.log(this.first)
       console.log(scrollY)
+      console.log(this.first.style.opacity)
       // decreases as user scrolls
-      this.first.value.style.opacity = (100 - ((scrollY + window.innerHeight) - this.first.value.offsetHeight)) / 100
+      //this.first.style.opacity = (100 - ((scrollY + window.innerHeight) - this.first.value.offsetHeight)) / 100
+      this.first.value.opacity = (100 - ((scrollY + window.innerHeight) - this.first.value.offsetHeight)) / 100
       // increases as user scrolls
       this.second.value.style.opacity = ((scrollY + window.innerHeight) - this.second.value.offsetTop) / 100
 
